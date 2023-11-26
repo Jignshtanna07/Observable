5fc2e90d95abf902c066065b5011477bc5b2c4c1
Jignesh Tanna <jtanna@asite.com>
2023-11-26 15:12:00 +0530

    Observable understanding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Observable';
+
+  // Observable like promises that are use to asychronously program.
+  // But Promises is Take full response form server. Promise provide us single response 
+  // But Observable stream a data request and then data moves in to chanks 
+
+  myObservable  =  new Observable ((observer)=>{
+    console.log("Observable is Starting")
+    observer.next("1")
+    observer.next("2")
+    observer.next("3")
+    observer.next("4")
+    console.log("Observable is Finished")
+  })
+  ngOnInit(){
+    this.myObservable.subscribe((res)=>{
+      console.log(res)
+    })
+  }
 }
